ea3adf2c6dc59357270a76ae68dd5d73de96aace
Naomi Rivera <naomirivera@naomis-macbook-air.local>
2019-02-12 01:44:41 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Naomi Rivera on 2/12/19.
+//  Copyright © 2019 Naomi Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
